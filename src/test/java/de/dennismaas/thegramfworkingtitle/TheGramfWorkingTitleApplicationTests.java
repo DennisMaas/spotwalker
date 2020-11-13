9974f0e7e765eb2f405eec37581d26616331159b
DennisMaas <mail@dennismaas.de>
2020-11-13 21:01:20 +0100

    add react app

---
+++ src/test/java/de/dennismaas/thegramfworkingtitle/TheGramfWorkingTitleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dennismaas.thegramfworkingtitle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGramfWorkingTitleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
