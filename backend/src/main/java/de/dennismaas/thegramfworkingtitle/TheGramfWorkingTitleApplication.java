a6593faebf28ead8c584453b02df1ee26bb04533
Dennis Maas <mail@dennismaas.de>
2020-11-14 21:29:54 +0100

    setup new spring projekt and new react app add .gitignore files

---
+++ backend/src/main/java/de/dennismaas/thegramfworkingtitle/TheGramfWorkingTitleApplication.java
@@ -0,0 +1,13 @@
+package de.dennismaas.thegramfworkingtitle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGramfWorkingTitleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGramfWorkingTitleApplication.class, args);
+	}
+
+}
