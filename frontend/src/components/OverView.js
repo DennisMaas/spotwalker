4c9fb21eec36dc4a1ff263b04b5fdce10ef030df
Dennis Maas <mail@dennismaas.de>
2020-11-18 14:08:50 +0100

    edit PlacesContextProvider.js

--- frontend/src/components/OverView.js
+++ frontend/src/components/OverView.js
@@ -4,12 +4,12 @@ import { Container, Grid } from '@material-ui/core';
 import PlacesContext from '../contexts/PlacesContext';
 
 export default function OverView() {
-  const { placeDb } = useContext(PlacesContext);
+  const { places } = useContext(PlacesContext);
 
   return (
     <Container disableGutters={false}>
       <Grid container spacing={2} direction={'row'} justify={'flex-start'}>
-        {placeDb.map((placeData) => (
+        {places?.map((placeData) => (
           <Grid item xs={12} sm={6} md={4} lg={3}>
             <PlaceCard
               key={placeData.id}
--- frontend/src/contexts/PlacesContextProvider.js
+++ frontend/src/contexts/PlacesContextProvider.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PlacesContext from './PlacesContext';
+import { getPlaces } from '../service/PlaceService';
 
 export default function PlacesContextProvider({ children }) {
-  return <PlacesContext.Provider value={{}}>{children}</PlacesContext.Provider>;
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    getPlaces().then(setPlaces).catch(console.log);
+  }, []);
+
+  return (
+    <PlacesContext.Provider value={{ places }}>
+      {children}
+    </PlacesContext.Provider>
+  );
 }
