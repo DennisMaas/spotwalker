e6184b2273e037ee5f7038d739bb65999f0d6749
Dennis Maas <mail@dennismaas.de>
2020-11-18 11:41:03 +0100

    add PlaceController

--- backend/src/main/java/de/dennismaas/thegramfworkingtitle/controller/PlaceController.java
+++ backend/src/main/java/de/dennismaas/thegramfworkingtitle/controller/PlaceController.java
@@ -1,5 +1,27 @@
 package de.dennismaas.thegramfworkingtitle.controller;
 
+import de.dennismaas.thegramfworkingtitle.model.Place;
+import de.dennismaas.thegramfworkingtitle.service.PlaceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/places")
 public class PlaceController {
+    private final PlaceService placeService;
+
+    @Autowired
+    public PlaceController(PlaceService placeService){
+        this.placeService = placeService;
+    }
+
+    @GetMapping("{placeId}")
+    public  Place getById(@PathVariable @NonNull String placeId) {
+        return placeService.findById(placeId);
+    }
 
 }
