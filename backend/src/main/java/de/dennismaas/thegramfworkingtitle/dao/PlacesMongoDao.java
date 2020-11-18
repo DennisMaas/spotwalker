be09301c989b3fcd2b6c9d7cf36c07c92692e47a
Dennis Maas <mail@dennismaas.de>
2020-11-18 11:34:05 +0100

    add PlacesMongoDao

--- backend/src/main/java/de/dennismaas/thegramfworkingtitle/dao/PlacesMongoDao.java
+++ backend/src/main/java/de/dennismaas/thegramfworkingtitle/dao/PlacesMongoDao.java
@@ -2,6 +2,9 @@ package de.dennismaas.thegramfworkingtitle.dao;
 import de.dennismaas.thegramfworkingtitle.model.Place;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 
 public interface PlacesMongoDao extends PagingAndSortingRepository<Place,String>{
+    List<Place> findAll();
 }
