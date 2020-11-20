58dbb42512e12c36e6abef073463362f8c0165fd
Dennis Maas <mail@dennismaas.de>
2020-11-20 17:10:36 +0100

    use iconswitcher, solved TypeError

--- frontend/src/components/cards/PlaceCard.js
+++ frontend/src/components/cards/PlaceCard.js
@@ -6,7 +6,7 @@ import CardHeader from '@material-ui/core/CardHeader';
 import Card from '@material-ui/core/Card';
 import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from 'react-router-dom';
-import IconSwitcher from './IconSwitcher';
+import PlaceTypeIcon from './PlaceTypeIcon';
 
 const useStyles = makeStyles({
   media: {
@@ -23,7 +23,7 @@ export default function PlaceCard({ placeData }) {
       <CardHeader
         avatar={
           <Avatar aria-label={placeData.type} className={classes.avatar}>
-            <IconSwitcher />
+            <PlaceTypeIcon type={placeData.type} />
           </Avatar>
         }
         title={placeData.title}
