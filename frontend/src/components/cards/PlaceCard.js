23aedd1c71e9a0c5aece7ceb438226c988d65a30
Dennis Maas <mail@dennismaas.de>
2020-11-22 18:22:52 +0100

    better link for google maps

--- frontend/src/components/cards/PlaceCard.js
+++ frontend/src/components/cards/PlaceCard.js
@@ -7,21 +7,27 @@ import Card from '@material-ui/core/Card';
 import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from 'react-router-dom';
 import PlaceTypeIcon from './PlaceTypeIcon';
-import Link from '@material-ui/core/Link';
 import MapOutlinedIcon from '@material-ui/icons/MapOutlined';
+import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles({
   media: {
     height: '27vh',
   },
+  mapicon: {},
 });
 
 export default function PlaceCard({ placeData }) {
   const classes = useStyles();
   const history = useHistory();
-  const {latitude, longitude} = placeData;
+  const { latitude, longitude } = placeData;
   const gMapUrl =
-    'https://www.google.com/maps/@' + latitude + ',' + longitude + ',' + '18z';
+    'https://www.google.com/maps/@?api=1&map_action=map&center=' +
+    latitude +
+    ',' +
+    longitude +
+    '&zoom=' +
+    '20';
 
   return (
     <Card className={classes.root}>
@@ -31,18 +37,13 @@ export default function PlaceCard({ placeData }) {
             <PlaceTypeIcon type={placeData.type} />
           </Avatar>
         }
-        title={placeData.title}
-        subheader={
-          <Link
-            color={'inherit'}
-            underline={'none'}
-            className={classes.link}
-            href={gMapUrl}
-          >
-            <MapOutlinedIcon fontSize={'inherit'} />
-            {placeData.street}
-          </Link>
+        action={
+          <IconButton href={gMapUrl} aria-label="maps">
+            <MapOutlinedIcon />
+          </IconButton>
         }
+        title={placeData.title}
+        subheader={placeData.street}
       />
       <CardActionArea onClick={() => history.push(`/places/${placeData.id}`)}>
         <CardMedia
