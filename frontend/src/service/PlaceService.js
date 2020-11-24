820cc264819ca3c9e7664fe7aa472a8f6fcf06c0
Dennis Maas <mail@dennismaas.de>
2020-11-24 18:09:16 +0100

    add axios post and put to PlaceService.js

--- frontend/src/service/PlaceService.js
+++ frontend/src/service/PlaceService.js
@@ -2,3 +2,91 @@ import axios from 'axios';
 
 export const getPlaces = () =>
   axios.get('/api/places').then((response) => response.data);
+
+export const addPlace = (
+  primaryPictureUrl,
+  type,
+  title,
+  street,
+  address,
+  latitude,
+  longitude,
+  placeDescription,
+  pictureDescription,
+  aperture,
+  focalLength,
+  shutterSpeed,
+  iso,
+  flash,
+  youTubeUrl,
+  extraOne,
+  extraTwo,
+  particularities
+) =>
+  axios
+    .post('/api/places', {
+      primaryPictureUrl,
+      type,
+      title,
+      street,
+      address,
+      latitude,
+      longitude,
+      placeDescription,
+      pictureDescription,
+      aperture,
+      focalLength,
+      shutterSpeed,
+      iso,
+      flash,
+      youTubeUrl,
+      extraOne,
+      extraTwo,
+      particularities,
+    })
+    .then((response) => response.data);
+
+export const updatePlace = (
+  id,
+  primaryPictureUrl,
+  type,
+  title,
+  street,
+  address,
+  latitude,
+  longitude,
+  placeDescription,
+  pictureDescription,
+  aperture,
+  focalLength,
+  shutterSpeed,
+  iso,
+  flash,
+  youTubeUrl,
+  extraOne,
+  extraTwo,
+  particularities
+) =>
+  axios
+    .put('/api/places' + id, {
+      id,
+      primaryPictureUrl,
+      type,
+      title,
+      street,
+      address,
+      latitude,
+      longitude,
+      placeDescription,
+      pictureDescription,
+      aperture,
+      focalLength,
+      shutterSpeed,
+      iso,
+      flash,
+      youTubeUrl,
+      extraOne,
+      extraTwo,
+      particularities,
+    })
+    .then((response) => response.data);
