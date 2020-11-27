29a59a9eaf8cf66f7c6ec42401aca5406e1b6bc3
Dennis Maas <mail@dennismaas.de>
2020-11-27 14:25:05 +0100

    maps can now accept some markers and save them in a state

--- frontend/src/components/commons/GetGoogleMap.js
+++ frontend/src/components/commons/GetGoogleMap.js
@@ -1,35 +1,59 @@
-import React from 'react';
-import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+import React, { useCallback, useState } from 'react';
+import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
 import Grid from '@material-ui/core/Grid';
 
 const libraries = ['places'];
+
 const mapContainerStyle = {
-  width: '100vw',
-  height: '100vh',
+  width: '400px',
+  height: '600px',
 };
-const center = { lat: 53.55009, lng: 9.991636 };
+
+const options = {
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+
+const center = {
+  lat: 53.55009,
+  lng: 9.991636,
+};
 
 export default function GetGoogleMap() {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
     libraries,
   });
+  const [markers, setMarkers] = useState([]);
+  const onMapClick = useCallback((event) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
 
   if (loadError) return 'Fehler beim Laden der Karte';
   if (!isLoaded) return 'Karte wird geladen';
   return (
-    <div>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <GoogleMap
-            mapContainerStyle={mapContainerStyle}
-            zoom={11}
-            center={center}
-          >
-            map
-          </GoogleMap>
-        </Grid>
-      </Grid>
-    </div>
+    <Grid item xs={12}>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={11}
+        center={center}
+        options={options}
+        onClick={onMapClick}
+      >
+        {markers?.map((marker) => (
+          <Marker
+            key={marker.time.toISOString()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+          />
+        ))}
+      </GoogleMap>
+    </Grid>
   );
 }
