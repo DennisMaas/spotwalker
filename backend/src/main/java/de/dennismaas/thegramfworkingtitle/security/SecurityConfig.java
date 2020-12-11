4395c805d22d016f5a27a9a49691131a751a47c0
Dennis Maas <mail@dennismaas.de>
2020-12-11 12:17:07 +0100

    add security config do get around cors problems

---
+++ backend/src/main/java/de/dennismaas/thegramfworkingtitle/security/SecurityConfig.java
@@ -0,0 +1,15 @@
+package de.dennismaas.thegramfworkingtitle.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests().antMatchers("/**").permitAll().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+    }
+}
