8cfe0faa7571ae80e01f6cf318dd95c513cdc24d
DennisMaas <mail@dennismaas.de>
2021-01-13 16:08:34 +0100

    remove double entry in pom.xml add annotation to CorsConfiguration

--- backend/src/main/java/de/dennismaas/thegramfworkingtitle/config/CorsConfiguration.java
+++ backend/src/main/java/de/dennismaas/thegramfworkingtitle/config/CorsConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-
+@Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -14,7 +14,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080", "http://localhost:3000",
+                        .allowedOrigins("http://localhost:8080", "http://localhost:3000", "http://127.0.0.1:3000", "https://localhost:8080", "https://localhost:3000", "https://127.0.0.1:3000",
                                 "https://spotwalker.de", "https://www.spotwalker.de", "https://api.spotwalker.de").allowCredentials(true).allowedMethods("*");
             }
         };
