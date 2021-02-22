ed4087e7711d7f940b0c29aa8633a10f963e42b4
DennisMaas <mail@dennismaas.de>
2021-02-22 19:00:57 +0100

    add jwtutils environment variable

--- backend/src/main/java/de/dennismaas/thegramfworkingtitle/security/JwtUtils.java
+++ backend/src/main/java/de/dennismaas/thegramfworkingtitle/security/JwtUtils.java
@@ -2,7 +2,7 @@ package de.dennismaas.thegramfworkingtitle.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -13,13 +13,16 @@ import java.util.Map;
 @Service
 public class JwtUtils {
 
+    @Value("jwt.secretkey")
+    private String secretKey;
+
     public String createToken(Map<String, Object> claims, String username){
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(Date.from(Instant.now()))
                 .setExpiration(Date.from(Instant.now().plus(Duration.ofMinutes(120))))
                 .setSubject(username)
-                .signWith(SignatureAlgorithm.HS256, "absolutely-secret-key")
+                .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
 
     }
