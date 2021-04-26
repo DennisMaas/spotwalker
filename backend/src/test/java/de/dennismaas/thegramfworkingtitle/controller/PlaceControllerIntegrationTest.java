dbda17b6190fa9ab419229178c39c7f09bc54355
DennisMaas <mail@dennismaas.de>
2021-04-26 11:47:02 +0200

    testing

--- backend/src/test/java/de/dennismaas/thegramfworkingtitle/controller/PlaceControllerIntegrationTest.java
+++ backend/src/test/java/de/dennismaas/thegramfworkingtitle/controller/PlaceControllerIntegrationTest.java
@@ -1,10 +1,13 @@
-/*
 package de.dennismaas.thegramfworkingtitle.controller;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import de.dennismaas.thegramfworkingtitle.dao.PlacesMongoDao;
 import de.dennismaas.thegramfworkingtitle.dto.AddPlaceDto;
 import de.dennismaas.thegramfworkingtitle.dto.UpdatePlaceDto;
 import de.dennismaas.thegramfworkingtitle.model.Place;
+import de.dennismaas.thegramfworkingtitle.utils.AmazonS3ClientUtils;
+import de.dennismaas.thegramfworkingtitle.utils.DateExpirationUtils;
 import de.dennismaas.thegramfworkingtitle.utils.IdUtils;
 import de.dennismaas.thegramfworkingtitle.utils.TimestampUtils;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,20 +17,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -37,10 +40,22 @@ class PlaceControllerIntegrationTest {
     private int port;
 
     @MockBean
-    private TimestampUtils mockedTimestampUtils;
+    private TimestampUtils timestampUtils;
 
     @MockBean
-    private IdUtils mockedIdUtils;
+    private IdUtils idUtils;
+
+    @MockBean
+    private AmazonS3 amazonS3;
+
+    @MockBean
+    private AmazonS3ClientUtils amazonS3ClientUtils;
+
+    @MockBean
+    private DateExpirationUtils expirationUtils;
+
+    @MockBean
+    private GeneratePresignedUrlRequest generatePresignedUrlRequest;
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -48,34 +63,38 @@ class PlaceControllerIntegrationTest {
     @Autowired
     private PlacesMongoDao placesMongoDao;
 
+    private String bucketName = "test-bucket";
+    private Date expiration = new Date(2030, Calendar.JANUARY,1);
+
+
     @BeforeEach
     public void setupDb(){
         placesMongoDao.deleteAll();
         placesMongoDao.saveAll(List.of(
-                new Place("someId", "someUrl",  "someImage", "someType", "someTitle",  "someStreet, someCity, someCountry", "someStreet", "someCity", "someCountry",56.000, 9.10, "somePlaceDesc", "somePicDesc", "someAperture", "someFocal", "someShutter","someIso", "someFlash", "someYT", "someX1", "someX2", "somePartic", Instant.parse("2016-11-30T18:35:24.00Z") ),
-                new Place("someId1", "someUrl1","someImage1", "someType1", "someTitle1", "someStreet1, someCity1, someCountry1","someStreet1", "someCity1", "someCountry1", 56.300, 3.10,"somePlaceDesc1", "somePicDesc1", "someAperture1", "someFocal1", "someShutter1","someIso1", "someFlash1", "someYT1", "someX11", "someX21", "somePartic1", Instant.parse("2017-11-30T18:35:24.00Z")),
-                new Place("someId2", "someUrl2", "someImage2","someType2", "someTitle2",  "someStreet2, someCity2, someCountry2", "someStreet2", "someCity2", "someCountry2",6.000, 9.330, "somePlaceDesc2", "somePicDesc2", "someAperture2", "someFocal2", "someShutter2","someIso2", "someFlash2", "someYT2", "someX12", "someX22", "somePartic2", Instant.parse("2018-11-30T18:35:24.00Z")),
-                new Place("someId3", "someUrl3", "someImage3","someType3", "someTitle3",  "someStreet3, someCity3, someCountry3", "someStreet3", "someCity3", "someCountry3",56.0300, 9.103, "somePlaceDesc3", "somePicDesc3", "someAperture3", "someFocal3", "someShutter3","someIso3", "someFlash3", "someYT3", "someX13", "someX23", "somePartic3", Instant.parse("2019-11-30T18:35:24.00Z"))
+                new Place("someId", "http://www.url.de",  "someImage", "someType", "someTitle",  "someStreet, someCity, someCountry", "someStreet", "someCity", "someCountry",56.000, 9.10, "somePlaceDesc", "somePicDesc", "someAperture", "someFocal", "someShutter","someIso", "someFlash", "someYT", "someX1", "someX2", "somePartic", Instant.parse("2016-11-30T18:35:24.00Z") ),
+                new Place("someId1", "http://www.url.de","someImage1", "someType1", "someTitle1", "someStreet1, someCity1, someCountry1","someStreet1", "someCity1", "someCountry1", 56.300, 3.10,"somePlaceDesc1", "somePicDesc1", "someAperture1", "someFocal1", "someShutter1","someIso1", "someFlash1", "someYT1", "someX11", "someX21", "somePartic1", Instant.parse("2017-11-30T18:35:24.00Z")),
+                new Place("someId2", "http://www.url.de", "someImage2","someType2", "someTitle2",  "someStreet2, someCity2, someCountry2", "someStreet2", "someCity2", "someCountry2",6.000, 9.330, "somePlaceDesc2", "somePicDesc2", "someAperture2", "someFocal2", "someShutter2","someIso2", "someFlash2", "someYT2", "someX12", "someX22", "somePartic2", Instant.parse("2018-11-30T18:35:24.00Z")),
+                new Place("someId3", "http://www.url.de", "someImage3","someType3", "someTitle3",  "someStreet3, someCity3, someCountry3", "someStreet3", "someCity3", "someCountry3",56.0300, 9.103, "somePlaceDesc3", "somePicDesc3", "someAperture3", "someFocal3", "someShutter3","someIso3", "someFlash3", "someYT3", "someX13", "someX23", "somePartic3", Instant.parse("2019-11-30T18:35:24.00Z"))
         ));
 
     }
-
     private String getPlaceUrl() { return "http://localhost:" + port + "/api/places";}
 
 
     @Test
-    public void testGetMapping() {
+    public void testGetMapping() throws MalformedURLException {
         // GIVEN
-        List<Place> stockPlaces = new ArrayList<>(List.of(
-                new Place("someId", "someUrl","someImage", "someType", "someTitle",  "someStreet, someCity, someCountry",  "someStreet", "someCity", "someCountry",56.000, 9.10, "somePlaceDesc", "somePicDesc", "someAperture", "someFocal", "someShutter", "someIso", "someFlash", "someYT", "someX1", "someX2", "somePartic", Instant.parse("2016-11-30T18:35:24.00Z")),
-                new Place("someId1", "someUrl1", "someImage1","someType1", "someTitle1", "someStreet1, someCity1, someCountry1", "someStreet1", "someCity1", "someCountry1", 56.300, 3.10,"somePlaceDesc1", "somePicDesc1", "someAperture1", "someFocal1", "someShutter1", "someIso1", "someFlash1", "someYT1", "someX11", "someX21", "somePartic1", Instant.parse("2017-11-30T18:35:24.00Z")),
-                new Place("someId2", "someUrl2", "someImage2","someType2", "someTitle2", "someStreet2, someCity2, someCountry2", "someStreet2", "someCity2", "someCountry2",6.000, 9.330, "somePlaceDesc2", "somePicDesc2", "someAperture2", "someFocal2", "someShutter2", "someIso2", "someFlash2", "someYT2", "someX12", "someX22", "somePartic2", Instant.parse("2018-11-30T18:35:24.00Z")),
-                new Place("someId3", "someUrl3","someImage3", "someType3", "someTitle3",  "someStreet3, someCity3, someCountry3", "someStreet3", "someCity3", "someCountry3",56.0300, 9.103, "somePlaceDesc3", "somePicDesc3", "someAperture3", "someFocal3", "someShutter3", "someIso3", "someFlash3", "someYT3", "someX13", "someX23", "somePartic3", Instant.parse("2019-11-30T18:35:24.00Z"))
-        ));
-        String url = getPlaceUrl();
+        List<Place> stockPlaces = List.of(
+                new Place("someId", "https://www.url.de","someImage", "someType", "someTitle",  "someStreet, someCity, someCountry",  "someStreet", "someCity", "someCountry",56.000, 9.10, "somePlaceDesc", "somePicDesc", "someAperture", "someFocal", "someShutter", "someIso", "someFlash", "someYT", "someX1", "someX2", "somePartic", Instant.parse("2016-11-30T18:35:24.00Z")),
+                new Place("someId1", "https://www.url.de", "someImage1","someType1", "someTitle1", "someStreet1, someCity1, someCountry1", "someStreet1", "someCity1", "someCountry1", 56.300, 3.10,"somePlaceDesc1", "somePicDesc1", "someAperture1", "someFocal1", "someShutter1", "someIso1", "someFlash1", "someYT1", "someX11", "someX21", "somePartic1", Instant.parse("2017-11-30T18:35:24.00Z")),
+                new Place("someId2", "http://www.url.de", "someImage2","someType2", "someTitle2", "someStreet2, someCity2, someCountry2", "someStreet2", "someCity2", "someCountry2",6.000, 9.330, "somePlaceDesc2", "somePicDesc2", "someAperture2", "someFocal2", "someShutter2", "someIso2", "someFlash2", "someYT2", "someX12", "someX22", "somePartic2", Instant.parse("2018-11-30T18:35:24.00Z")),
+                new Place("someId3", "http://www.url.de","someImage3", "someType3", "someTitle3",  "someStreet3, someCity3, someCountry3", "someStreet3", "someCity3", "someCountry3",56.0300, 9.103, "somePlaceDesc3", "somePicDesc3", "someAperture3", "someFocal3", "someShutter3", "someIso3", "someFlash3", "someYT3", "someX13", "someX23", "somePartic3", Instant.parse("2019-11-30T18:35:24.00Z"))
+        );
+        when(expirationUtils.getExpirationTime()).thenReturn(expiration);
+        when(amazonS3.generatePresignedUrl(bucketName, "someImage", expiration, com.amazonaws.HttpMethod.GET)).thenReturn(new URL("http://www.url.de"));
 
         // WHEN
-        ResponseEntity<Place[]> response = restTemplate.getForEntity(url, Place[].class);
+        ResponseEntity<Place[]> response = restTemplate.getForEntity(getPlaceUrl(), Place[].class);
 
         //THEN
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
@@ -152,8 +171,8 @@ class PlaceControllerIntegrationTest {
                 "someX2",
                 "somePartic"
                 );
-        when(mockedIdUtils.generateId()).thenReturn("some generated Id");
-        when(mockedTimestampUtils.generateTimestampEpochSeconds()).thenReturn(Instant.parse("2018-11-30T18:35:24.00Z"));
+        when(idUtils.generateId()).thenReturn("some generated Id");
+        when(timestampUtils.generateTimestampEpochSeconds()).thenReturn(Instant.parse("2018-11-30T18:35:24.00Z"));
 
         //WHEN
         HttpEntity<AddPlaceDto> entity = new HttpEntity<>(placeToAdd);
@@ -198,7 +217,7 @@ class PlaceControllerIntegrationTest {
         long currentTimeInSeconds = Instant.now().getEpochSecond();
         Instant now = Instant.ofEpochSecond(currentTimeInSeconds);
 
-        when(mockedTimestampUtils.generateTimestampEpochSeconds()).thenReturn(now);
+        when(timestampUtils.generateTimestampEpochSeconds()).thenReturn(now);
 
         UpdatePlaceDto updatePlace = UpdatePlaceDto.builder()
                 .id("someId")
@@ -270,7 +289,7 @@ class PlaceControllerIntegrationTest {
         long currentTimeInSeconds = Instant.now().getEpochSecond();
         Instant now = Instant.ofEpochSecond(currentTimeInSeconds);
 
-        when(mockedTimestampUtils.generateTimestampEpochSeconds()).thenReturn(now);
+        when(timestampUtils.generateTimestampEpochSeconds()).thenReturn(now);
 
         UpdatePlaceDto updatePlace = UpdatePlaceDto.builder()
                 .id("mismatchedId")
@@ -337,4 +356,3 @@ class PlaceControllerIntegrationTest {
     }
 
 }
-*/
--- backend/src/test/java/de/dennismaas/thegramfworkingtitle/service/PlaceServiceTest.java
+++ backend/src/test/java/de/dennismaas/thegramfworkingtitle/service/PlaceServiceTest.java
@@ -12,7 +12,6 @@ import de.dennismaas.thegramfworkingtitle.utils.IdUtils;
 import de.dennismaas.thegramfworkingtitle.utils.TimestampUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -24,12 +23,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-
-
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 
@@ -41,28 +36,25 @@ class PlaceServiceTest {
     final IdUtils idUtils = mock(IdUtils.class);
     final TimestampUtils timestampUtils = mock(TimestampUtils.class);
     final DateExpirationUtils expirationUtils = mock(DateExpirationUtils.class);
-    final GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest("test-bucket", "someImage" );
-    final PlaceService placeService = new PlaceService(placesMongoDao, amazonS3, idUtils, timestampUtils, expirationUtils);
+    final PlaceService placeService = new PlaceService(
+            placesMongoDao,
+            amazonS3,
+            idUtils,
+            timestampUtils,
+            expirationUtils);
+
 
     @Test
     @DisplayName("get list of all places")
     void itShouldGetPlaces() throws MalformedURLException {
         //GIVEN
         List<Place> places = (List.of(
-                new Place("someId", "http://www.url.de", "someImage", "someType", "someTitle", "someStreet, someCity, someCountry", "someStreet", "someCity", "someCountry", 56.000, 9.10, "somePlaceDesc", "somePicDesc", "someAperture", "someFocal", "someShutter", "someIso", "someFlash", "someYT", "someX1", "someX2", "somePartic", Instant.parse("2016-11-30T18:35:24.00Z")),
+                new Place("someId", "http://www.url.de", "someImage", "someType", "someTitle", "someStreet, someCity, someCountry", "someStreet", "someCity", "someCountry", 56.300, 3.10, "somePlaceDesc", "somePicDesc", "someAperture", "someFocal", "someShutter", "someIso", "someFlash", "someYT", "someX1", "someX2", "somePartic", Instant.parse("2017-11-30T18:35:24.00Z")),
                 new Place("someId1", "http://www.url1.de", "someImage1", "someType1", "someTitle1", "someStreet1, someCity1, someCountry1", "someStreet1", "someCity1", "someCountry1", 56.300, 3.10, "somePlaceDesc1", "somePicDesc1", "someAperture1", "someFocal1", "someShutter1", "someIso1", "someFlash1", "someYT1", "someX11", "someX21", "somePartic1", Instant.parse("2017-11-30T18:35:24.00Z")),
                 new Place("someId2", "http://www.url2.de", "someImage2", "someType2", "someTitle2", "someStreet2, someCity2, someCountry2", "someStreet2", "someCity2", "someCountry2", 6.000, 9.330, "somePlaceDesc2", "somePicDesc2", "someAperture2", "someFocal2", "someShutter2", "someIso2", "someFlash2", "someYT2", "someX12", "someX22", "somePartic2", Instant.parse("2018-11-30T18:35:24.00Z")),
                 new Place("someId3", "http://www.ur13.de", "someImage3", "someType3", "someTitle3", "someStreet3, someCity3, someCountry3", "someStreet3", "someCity3", "someCountry3", 56.0300, 9.103, "somePlaceDesc3", "somePicDesc3", "someAperture3", "someFocal3", "someShutter3", "someIso3", "someFlash3", "someYT3", "someX13", "someX23", "somePartic3", Instant.parse("2019-11-30T18:35:24.00Z"))));
 
-        Date expiration = new Date(2030, Calendar.JANUARY, 1);
-        GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest("test-bucket", "someImage").withMethod(HttpMethod.GET).withExpiration(expiration);
-
         when(placesMongoDao.findAll()).thenReturn(places);
-        when(expirationUtils.getExpirationTime()).thenReturn(expiration);
-        when(amazonS3.generatePresignedUrl(request)).thenReturn(new URL("http://www.url.de"));
-        when(amazonS3.generatePresignedUrl(request)).thenReturn(new URL("http://www.url1.de"));
-        when(amazonS3.generatePresignedUrl(request)).thenReturn(new URL("http://www.url2.de"));
-        when(amazonS3.generatePresignedUrl(request)).thenReturn(new URL("http://www.url3.de"));
 
         //WHEN
         List<Place> foundPlaces = placeService.getPlaces();
@@ -78,7 +70,7 @@ class PlaceServiceTest {
         String placeId = "uniqueId";
 
         Place expectedPlace = new Place(
-                "someId1", "someUrl1", "someImage", "someType1", "someTitle1", "someAddress1",   "someStreet1", "someCity1", "someCountry1", 56.000, 9.10, "somePlaceDesc1", "somePicDesc1", "someAperture1", "someFocal1", "someShutter1","someIso1", "someFlash1", "someYT1", "someX11", "someX21", "somePartic1", Instant.parse("2017-11-30T18:35:24.00Z")
+                "someId1", "http://www.url.de", "someImage", "someType1", "someTitle1", "someAddress1",   "someStreet1", "someCity1", "someCountry1", 56.000, 9.10, "somePlaceDesc1", "somePicDesc1", "someAperture1", "someFocal1", "someShutter1","someIso1", "someFlash1", "someYT1", "someX11", "someX21", "somePartic1", Instant.parse("2017-11-30T18:35:24.00Z")
         );
             when(placesMongoDao.findById("uniqueId")).thenReturn(Optional.of(expectedPlace));
 
@@ -108,70 +100,72 @@ class PlaceServiceTest {
 
     }
 
+
     @Test
+    @DisplayName("Add should create new place in db")
     void add() throws MalformedURLException {
         //GIVEN
         String placeId = "uniqueId";
-        String url = "http://url.de";
+        String primaryImageUrl = "http://www.url.de";
         Instant timestamp = Instant.parse("2020-11-24T08:00:00Z");
         Date expiration = new Date(2030, Calendar.JANUARY, 1);
-        GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest("test-bucket", "someImage").withMethod(HttpMethod.GET).withExpiration(expiration);
-
 
         AddPlaceDto placeToBeAdded = new AddPlaceDto(
-                "someImageName",
-                "someType",
-                "someTitle",
-                "someStreet, someCity, someCountry",
-                "someStreet",
-                "someCity",
-                "someCountry",
-                46.300,
-                29.20,
-                "somePlaceDesc",
-                "somePicDesc",
-                "someAperture",
-                "someFocal",
-                "someShutter",
-                "someIso",
-                "someFlash",
-                "someYT",
-                "someEx1",
-                "someEx2",
-                "somePart"
-                );
-
-
-        Place newPlace = new Place(
-                placeId,
-                url,
-                "someImageName",
-                "someType",
-                "someTitle",
-                "someStreet, someCity, someCountry",
-                "someStreet",
-                "someCity",
-                "someCountry",
-                46.300,
-                29.20,
-                "somePlaceDesc",
-                "somePicDesc",
-                "someAperture",
-                "someFocal",
-                "someShutter",
-                "someIso",
-                "someFlash",
-                "someYT",
-                "someEx1",
-                "someEx2",
-                "somePart",
-                timestamp
+                "primaryImageName",
+                "type",
+                "title",
+                "street, city, country",
+                "street",
+                "city",
+                "country",
+                42.0,
+                66.6,
+                "placeDescription",
+                "pictureDescription",
+                "aperture",
+                "focalLength",
+                "shutterSpeed",
+                "iso",
+                "flash",
+                "youTubeUrl",
+                "extraOne",
+                "extraTwo",
+                "particularities"
         );
 
+        Place newPlace = Place.builder()
+                .id(placeId)
+                .primaryImageName("primaryImageName")
+                .primaryImageUrl(primaryImageUrl)
+                .type("type")
+                .title("title")
+                .address("street, city, country")
+                .street("street")
+                .city("city")
+                .country("country")
+                .lat(42.0)
+                .lng(66.6)
+                .placeDescription("placeDescription")
+                .pictureDescription("pictureDescription")
+                .aperture("aperture")
+                .focalLength("focalLength")
+                .shutterSpeed("shutterSpeed")
+                .iso("iso")
+                .flash("flash")
+                .youTubeUrl("youTubeUrl")
+                .extraOne("extraOne")
+                .extraTwo("extraTwo")
+                .particularities("particularities")
+                .timestamp(timestamp)
+                .build();
+
+        when(expirationUtils.getExpirationTime()).thenReturn(expiration);
         when(idUtils.generateId()).thenReturn(placeId);
-        when(amazonS3.generatePresignedUrl(request)).thenReturn(new URL(url));
         when(timestampUtils.generateTimestampEpochSeconds()).thenReturn(timestamp);
         when(placesMongoDao.save(newPlace)).thenReturn(newPlace);
+        when(amazonS3.generatePresignedUrl(new GeneratePresignedUrlRequest("test-bucket", "primaryImageName").withMethod(HttpMethod.GET).withExpiration(expiration))).thenReturn(new URL(primaryImageUrl));
+        when(placesMongoDao.save(newPlace)).thenReturn(newPlace);
+
 
         //WHEN
         Place result = placeService.add(placeToBeAdded);
@@ -182,91 +176,101 @@ class PlaceServiceTest {
     }
 
     @Test
-    void update() {
+    void update() throws MalformedURLException{
         //GIVEN
         String placeId = "uniqueId";
+        String primaryImageUrl = "http://www.url.de";
         Instant timestamp = Instant.parse("2020-11-24T08:00:00Z");
+        Date expiration = new Date(2030, Calendar.JANUARY, 1);
 
-        UpdatePlaceDto update = new UpdatePlaceDto(placeId,
-                "someImage",
+        UpdatePlaceDto updatePlaceDto = new UpdatePlaceDto(
+                placeId,
+                "oldPrimaryImageName",
                 "soon to be updated Type1",
                 "soon to be updated Title1",
                 "soon to be updated Street1, soon to be updated City1, soon to be updated Country1",
                 "soon to be updated Street1",
                 "soon to be updated City1",
                 "soon to be updated Country1",
-                46.300,
-                29.20,
+                42.0,
+                66.6,
                 "soon to be updated PlaceDesc1",
-                "somePicDesc1",
-                "someAperture1",
-                "someFocal1",
-                "someShutter1",
-                "someIso1",
-                "someFlash1",
-                "someYT1",
-                "someX11",
-                "someX21",
-                "somePartic1");
-        Place oldPlace = new Place(placeId,
-                "old Url1",
-                "someImage",
-                "old Type1",
-                "old Title1",
-                "oldStreet1, oldCity1, oldCountry1",
-                "oldStreet1",
-                "oldCity1",
-                "oldCountry1",
-                56.300,
-                9.20,
-                "oldlaceDesc1",
-                "oldPicDesc1",
-                "oldAperture1",
-                "oldocal1",
-                "oldShutter1",
-                "oldIso1",
-                "oldFlash1",
-                "oldYT1",
-                "oldX11",
-                "oldX21",
-                "oldPartic1",
-                timestamp);
-        Place updatedPlace = new Place(placeId,
-                "old Url1",
-                "someImage",
-                "soon to be updated Type1",
-                "soon to be updated Title1",
-                "soon to be updated Street1, soon to be updated City1, soon to be updated Country1",
-                "soon to be updated Street1",
-                "soon to be updated City1",
-                "soon to be updated Country1",
-                46.300,
-                29.20,
-                "soon to be updated PlaceDesc1",
-                "somePicDesc1",
-                "someAperture1",
-                "someFocal1",
-                "someShutter1",
-                "someIso1",
-                "someFlash1",
-                "someYT1",
-                "someX11",
-                "someX21",
-                "somePartic1",
-                timestamp);
+                "oldPictureDescription",
+                "oldAperture",
+                "oldFocalLength",
+                "oldShutterSpeed",
+                "oldIso",
+                "oldFlash",
+                "oldYouTubeUrl",
+                "oldExtraOne",
+                "oldExtraTwo",
+                "oldParticularities"
+        );
 
+        Place oldPlace = Place.builder()
+                .id(placeId)
+                .primaryImageName("oldPrimaryImageName")
+                .primaryImageUrl(primaryImageUrl)
+                .type("oldType")
+                .title("oldTtitle")
+                .address("oldStreet, city, country")
+                .street("oldStreet")
+                .city("city")
+                .country("country")
+                .lat(42.0)
+                .lng(66.6)
+                .placeDescription("oldPlaceDescription")
+                .pictureDescription("oldPictureDescription")
+                .aperture("oldAperture")
+                .focalLength("oldFocalLength")
+                .iso("oldIso")
+                .shutterSpeed("oldShutterSpeed")
+                .flash("oldFlash")
+                .youTubeUrl("oldYouTubeUrl")
+                .extraOne("oldExtraOne")
+                .extraTwo("oldExtraTwo")
+                .particularities("oldParticularities")
+                .timestamp(timestamp)
+                .build();
+
+        Place updatedPlace = Place.builder()
+                .id(placeId)
+                .primaryImageName("oldPrimaryImageName")
+                .primaryImageUrl(primaryImageUrl)
+                .type("soon to be updated Type1")
+                .title("soon to be updated Title1")
+                .address("soon to be updated Street1, soon to be updated City1, soon to be updated Country1")
+                .street("soon to be updated Street1")
+                .city("soon to be updated City1")
+                .country("soon to be updated Country1")
+                .lat(42.0)
+                .lng(66.6)
+                .placeDescription("soon to be updated PlaceDesc1")
+                .pictureDescription("somePicDesc1")
+                .aperture("oldAperture")
+                .focalLength("oldFocalLength")
+                .iso("oldIso")
+                .shutterSpeed("oldShutterSpeed")
+                .flash("oldFlash")
+                .youTubeUrl("oldYouTubeUrl")
+                .extraOne("oldExtraOne")
+                .extraTwo("oldExtraTwo")
+                .particularities("oldParticularities")
+                .timestamp(timestamp)
+                .build();
+
+        when(idUtils.generateId()).thenReturn(placeId);
         when(placesMongoDao.findById(placeId)).thenReturn(Optional.of(oldPlace));
         when(timestampUtils.generateTimestampEpochSeconds()).thenReturn(timestamp);
+        when(amazonS3.generatePresignedUrl(new GeneratePresignedUrlRequest("test-bucket", "primaryImageName").withMethod(HttpMethod.GET).withExpiration(expiration))).thenReturn(new URL(primaryImageUrl));
         when(placesMongoDao.save(updatedPlace)).thenReturn(updatedPlace);
 
         //WHEN
-        Place result = placeService.update(update,placeId);
+        Place result = placeService.update(updatePlaceDto,placeId);
 
         //THEN
         assertThat(result, is(updatedPlace));
         verify(placesMongoDao).save(updatedPlace);
-
-
     }
 
 
